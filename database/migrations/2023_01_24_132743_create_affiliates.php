3c40d9b2fbeb63fb67fc2bf8eab1ecf739f9179e
Antonio Munoz <ajmunbal@gmail.com>
2023-01-24 14:41:37 +0100

    Added seeder

---
+++ database/migrations/2023_01_24_132743_create_affiliates.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('affiliates', function (Blueprint $table) {
+            $table->id();
+            $table->string('web_name');
+            $table->string('url');
+            $table->string('nats_webcam');
+            $table->string('nats_web');
+            $table->string('assets');
+            $table->string('google_analytics');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('affiliates');
+    }
+};
---
+++ database/seeders/AffiliatesSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\src\domain\Affiliate;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AffiliatesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $affiliate = new Affiliate();
+        $affiliate->setWebNameAttribute('Babosas')
+            ->setUrlAttribute('http://babosas.com')
+            ->setNatsWebcamAttribute('babosas12345')
+            ->setNatsWebAttribute('babosas67890')
+            ->setAssetsAttribute('babosas')
+            ->setGoogleAnalyticsAttribute('AU-1XXXX');
+        $affiliate->save();
+
+        $affiliate = new Affiliate();
+        $affiliate->setWebNameAttribute('ConejoX')
+            ->setUrlAttribute('http://conejox.com')
+            ->setNatsWebcamAttribute('conejox12345')
+            ->setNatsWebAttribute('conejox67890')
+            ->setAssetsAttribute('conejox')
+            ->setGoogleAnalyticsAttribute('AU-2XXXX');
+        $affiliate->save();
+
+        $affiliate = new Affiliate();
+        $affiliate->setWebNameAttribute('Cerdas')
+            ->setUrlAttribute('http://cerdas.com')
+            ->setNatsWebcamAttribute('cerdas12345')
+            ->setNatsWebAttribute('cerdas67890')
+            ->setAssetsAttribute('cerdas')
+            ->setGoogleAnalyticsAttribute('AU-3XXXX');
+        $affiliate->save();
+    }
+}
